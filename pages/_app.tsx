f884d2df5a411829e9a8b7c1e8b8855a8f3c21df
Vikrant Bhat <vik.bhat223@gmail.com>
2021-02-10 06:51:31 +0530

    Chakra ui setup

---
+++ pages/_app.tsx
@@ -0,0 +1,12 @@
+import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+
+function MyApp({ Component, pageProps }: AppProps) {
+	return (
+		<ChakraProvider resetCSS>
+			<Component {...pageProps} />
+		</ChakraProvider>
+	);
+}
+
+export default MyApp;
